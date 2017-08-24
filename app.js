64c657725a3db624a4d1e0e7e119f1d2f5c465af
conyanco <n.namatame@gmail.com>
2017-08-25 07:27:51 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var request = require('request');
-request('https://api.a3rt.recruit-tech.co.jp/proofreading/v1/typo? 
-apikey=hZKOAncBCO4frSyuxZF7JR9l4j8sTBUE& 
-sentence=', function (error, response, body) {
+request('https://api.a3rt.recruit-tech.co.jp/proofreading/v1/typo?apikey=hZKOAncBCO4frSyuxZF7JR9l4j8sTBUE&sentence=', function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML.
