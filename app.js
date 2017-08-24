fbbbccf80a0f4092879673a6eca5ad5bafc7e260
conyanco <n.namatame@gmail.com>
2017-08-25 08:10:06 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-var request = require('request');
+'use strict';
+const api = require('api');
+const request = require('request');
 request('https://api.a3rt.recruit-tech.co.jp/proofreading/v1/typo?apikey=hZKOAncBCO4frSyuxZF7JR9l4j8sTBUE&sentence=', function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
